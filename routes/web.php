126cd961ce47651cc229ea6a99082cbcf9104361
Affodilaj Fadil <fadhilaainsalsabilla@mail.ugm.ac.id>
2023-09-26 00:46:41 -0700

    ya

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+//route resource
+Route::resource('/', \App\Http\Controllers\PostController::class);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+Route::put('/posts/{post}', [PostController::class, 'update'])->name('posts.update');
+
